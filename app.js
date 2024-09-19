1e109eebfdb5c7225e2c8880aa761310ae27e8b3
pranshul gupta <pranshulg01@gmail.com>
2024-09-19 09:02:53 +0530

    resolving issue of not loading .env

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const dotenv = require('dotenv');
 const FoodItemRoutes = require('./routes/FoodItem.route')
 
 // Load environment variables from the configs/.env file
-dotenv.config({ path: './configs/.env' });
-
-console.log('MONGO_URI:', process.env.MONGO_URI);  // Check if it prints correctly
-
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, 'configs/.env') });
 
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/IngreDientify';
 
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/IngreDientify';
+// 'mongodb+srv://pranshulg01:BByIuEIX5TeoN2z7@ingredientify.on2eg.mongodb.net/?retryWrites=true&w=majority&appName=IngreDientify'
+
+
+console.log(mongoURI);  // Check if it prints correctly
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
